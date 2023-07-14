d27dcbe2adfd3fda64bae277a57f99f5e769eac8
Dhiraj Mahadik <dhirajmahadik9221@gmail.com>
2023-07-15 02:20:26 +0530

    NA

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Components/Home";
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import Login from "./Components/Login";
-import Register from "./Components/Register";
+// import Register from "./Components/Register";
 import ForgetPass from "./Components/ForgetPass";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
    <Routes>
     <Route path="/" element={<Login/>}/>
     <Route path="/forget-password" element={<ForgetPass/>}/>
-    <Route path="/register" element={<Register/>}/>
+    {/* <Route path="/register" element={<Register/>}/> */}
       <Route path="/user-profile" element={<Home/>}/>
    </Routes>
    </BrowserRouter>
